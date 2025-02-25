bd4ee5e340f4c46ae91002b4acdea56623b1c15d
NARESHRAJ R <91484269+NARESHRAJ701@users.noreply.github.com>
2025-02-25 17:16:02 +0530

    new ai

---
+++ sites/2YopypW6BkZvwTehXn9hiO/chunk-TXKF355L.mjs
@@ -0,0 +1,3 @@
+function a(e,t){return{customHTMLHeadEnd:'<meta name="google-site-verification" content="9Z_nHq1cwK4OKtQVZ8B-GeI0WjsDuak2e9aoqu3xyfs">',description:`Moondream is a highly efficient open-source vision language model that combines powerful image understanding capabilities with a remarkably small footprint.
+`,favicon:"https://framerusercontent.com/assets/hbGcQvW4B4Y2DER6ryRSCZZ3UnQ.png",robots:"max-image-preview:large",title:"Moondream AI"}}export{a};
+//# sourceMappingURL=chunk-TXKF355L.mjs.map
